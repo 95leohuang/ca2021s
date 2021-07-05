24062547c243914f6d2b8513bf39b27eb0e2969b
leohuang <leohuang95@gmail.com>
2021-07-05 15:25:59 +0800

    【Feat】 - homepage nav scroller、video dont auto play、visitor counter RWD、code beautify

--- main.js
+++ main.js
@@ -59,53 +59,17 @@ function randomColor() {
     }
     return color;
 }
-// $(document).ready(function() {
+
 for (var i = 1; i <= 6; i++) {
-    // alert('#lab' + i)
     $('#lab0' + i).css('background-color', randomColor());
 }
-// })
 
-(function($) {
-    $.fn.arctic_scroll = function(options) {
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        };
-        var options = $.extend(defaults, options);
 
-        options.elem.click(function(event) {
-            event.preventDefault();
-            var offset = $(this).attr("data-offset") ?
-                $(this).attr("data-offset") :
-                false,
-                position = $(this).attr("data-position") ?
-                $(this).attr("data-position") :
-                false;
-            if (offset) {
-                var toMove = parseInt(offset);
-                $("html,body")
-                    .stop(true, false)
-                    .animate({ scrollTop: $(this.hash).offset().top + toMove }, options.speed);
-            } else if (position) {
-                var toMove = parseInt(position);
-                $("html,body")
-                    .stop(true, false)
-                    .animate({ scrollTop: toMove }, options.speed);
-            } else {
-                $("html,body")
-                    .stop(true, false)
-                    .animate({ scrollTop: $(this.hash).offset().top }, options.speed);
-            }
-        });
-    };
-})(jQuery);
-
-$(function() {
-    $(".scroller").arctic_scroll({
-        speed: 600
-    });
-});
+function scrolltoID(id, speed = 500) {
+    $('html, body').animate({
+        scrollTop: $('#' + id).offset().top - $("#nav").height()
+    }, speed);
+}
 
 
 function readMore() {
